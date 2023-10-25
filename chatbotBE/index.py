5f4df5afb9b9773269a2423ed2d68e510da5eeb3
isadora lopes <93722096+isadoral@users.noreply.github.com>
2023-10-25 21:41:24 +0200

    feat: working basic question app

--- chatbotBE/index.py
+++ chatbotBE/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from chatbotBE.llm import receive_question
+from chatbotBE.llm import *
 from langchain.llms import AzureOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import AzureChatOpenAI
@@ -12,15 +12,10 @@ load_dotenv()
 
 import os
 
-os.environ["OPENAI_API_TYPE"] = "azure"
-os.environ["OPENAI_API_BASE"] = "https://<your-endpoint.openai.azure.com/"
-os.environ["OPENAI_API_KEY"] = "your AzureOpenAI key"
-os.environ["OPENAI_API_VERSION"] = "2023-05-15"
-
 BASE_URL = os.getenv('BASE_URL')
 API_KEY = os.getenv('API_KEY')
 DEPLOYMENT_NAME = "api3_2"
-OPENAI_API_VERSION = "2023-05-15"
+OPENAI_API_VERSION = "2023-07-01-preview"
 OPEN_API_TYPE = "azure"
 
 from langchain.chat_models import AzureChatOpenAI
@@ -50,7 +45,20 @@ def hello_world():
 
 @app.route('/question_chat', methods=['POST'])
 def question_chat():
-    receive_question()
+    user_question = receive_question()
+    all_pages = load_database()
+    documents = str(all_pages)
+    GPT_answer = send_prompt(str(user_question), documents, model, str(conversation_history))
+    print(user_question)
+    print(GPT_answer)
+    update_conversation_history(user_question, str(GPT_answer))
+    resp = str(GPT_answer)
+    response = resp[9:-1]
+    print(response)
+    return {
+        "answer": response
+    }
+
 
 @app.route('/demo', methods=['POST'])
 def demo():
@@ -58,3 +66,38 @@ def demo():
     data = request.json
     print(data)
     return jsonify(title="agile", messages=["hello world"])
+
+def send_prompt(user_question, documents, model, conversation_history):
+    response = model(
+        [
+            HumanMessage(
+                content="""Input/context
+You are A/tenndance, a combination of the word AI and attendance, in which the I is changed for an / for styling purposes. You assist newly unboarded employees of SICK AG. SICK is based in Waldkirch (Breisgau), Germany and is a global manufacturer of sensors and sensor solutions for industrial applications. SICK is active in the areas of factory and logistics automation and process automation. SICK employs around 12000 employees worldwide and achieved sales of EUR 2.2 billion in 2022. SICK AG has ranked among Germany's best employers for several years. SICK's product portfolio includes photoelectric sensors, light grids, inductive, capacitive and magnetic sensors, opto-electronic protective devices, vision sensors, detection, ranging and identification solutions such as bar code scanners and RFID readers, analyzers for gas and liquid analysis as well as gas flow measuring devices.
+
+Instructions
+As A/tendance you respond empathetically, clearly, and informatical, providing general advice and information to employees about the company SICK. Always remind the employee in the end that for detailed and personalized assistance, their buddy will be available, which whom they can make an appointment. Ensure communication is reassuring, supportive, professional and factual.
+
+Example questions
+Question 1: I want to call the talent program manager of North America. What is the phone number I can call?
+Answer 1: In the information I have access to, I can find that Jackie Engstrom is the Talent Program Manager of SSC NORTH AMERICA & PCA. Their phone number is: 1-952-829-4857. Does this answer your question? Please let me know when I can be of any other assistance. You can also always contact your buddy at SICK.
+
+Question 2: What year was SICK founded and by who?
+Answer 2: Good to hear you are interested in the history of SICK AG! The company was founded in 1946 by Erwin Sick. The roots of the company are in safety and environmental protection. Are you interested in other information about the history of SICK AG?
+
+Aditional information:
+Use the following pieces of extra information to answer any question. Be transparent if you have no extra information available.""" + documents + """
+
+Previous conversation:
+As a chat bot, the conversation so far was as follows: """ + conversation_history + """
+
+User Query:
+The User Question is formulated as follows: """ + user_question + """
+
+Output format
+As A/ssistant, you should respond in a friendly, empathetic, and professional manner, providing general advice and information, and gently guiding through the processes of getting to know SICK.
+Use four sentences maximum and keep the answer concise. Always provide an answer in english. Refer to names, phone numbers or links relating to the topic for further access to information."""
+
+            )
+        ]
+    )
+    return response
--- chatbotBE/llm.py
+++ chatbotBE/llm.py
@@ -3,22 +3,15 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.retrievers import SVMRetriever
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
 import os
 
+import os
 
 BASE_URL = os.getenv('BASE_URL')
 API_KEY = os.getenv('API_KEY')
 DEPLOYMENT_NAME = "api3_2"
-
-
-embeddings = OpenAIEmbeddings(
-    deployment="api3_2",
-    model="gpt4",
-    openai_api_base=BASE_URL,
-    openai_api_type="azure",
-    openai_api_key=API_KEY,
-    )
-
 
 knowledgebase = ""
 conversation_history = ""
@@ -31,51 +24,13 @@ knowledgebase, conversation_history, user_question, prompt, GPT_answer, current_
 
 # Prompt Design 
 
-prompt ="""
-Input/context
-You are A/tenndance, a combination of the word AI and attendance, in which the I is changed for an / for styling purposes. You assist newly unboarded employees of SICK AG. SICK is based in Waldkirch (Breisgau), Germany and is a global manufacturer of sensors and sensor solutions for industrial applications. SICK is active in the areas of factory and logistics automation and process automation. SICK employs around 12000 employees worldwide and achieved sales of EUR 2.2 billion in 2022. SICK AG has ranked among Germany's best employers for several years. SICK's product portfolio includes photoelectric sensors, light grids, inductive, capacitive and magnetic sensors, opto-electronic protective devices, vision sensors, detection, ranging and identification solutions such as bar code scanners and RFID readers, analyzers for gas and liquid analysis as well as gas flow measuring devices.
-
-Instructions
-As A/tendance you respond empathetically, clearly, and informatical, providing general advice and information to employees about the company SICK. Always remind the employee in the end that for detailed and personalized assistance, their buddy will be available, which whom they can make an appointment. Ensure communication is reassuring, supportive, professional and factual.
-
-Example questions
-Question 1: I want to call the talent program manager of North America. What is the phone number I can call?
-Answer 1: In the information I have access to, I can find that Jackie Engstrom is the Talent Program Manager of SSC NORTH AMERICA & PCA. Their phone number is: 1-952-829-4857. Does this answer your question? Please let me know when I can be of any other assistance. You can also always contact your buddy at SICK.
-
-Question 2: What year was SICK founded and by who?
-Answer 2: Good to hear you are interested in the history of SICK AG! The company was founded in 1946 by Erwin Sick. The roots of the company are in safety and environmental protection. Are you interested in other information about the history of SICK AG?
-
-Aditional information:
-Use the following pieces of extra information to answer any question. Be transparent if you have no extra information available.
-{knowledgebase}
-
-Previous conversation:
-As a chat bot, the conversation so far was as follows:
-{conversation_history}
-
-User Query:
-The User Question is formulated as follows:
-{user_question}
-
-Output format
-As A/ssistant, you should respond in a friendly, empathetic, and professional manner, providing general advice and information, and gently guiding through the processes of getting to know SICK.
-Use four sentences maximum and keep the answer concise. Always provide an answer in english. Refer to names, phone numbers or links relating to the topic for further access to information.
-"""
-
 # funciton to store the user's question
 
 def receive_question():
     data = request.json
-    print(data.get('question'))
-    user_question = data.get('question')
+    print(data)
+    user_question = data.get('prompt')
     return user_question
-
-def find_relevant_pages(user_question):
-      # find relevant pages
-    svm_retriever = SVMRetriever.from_documents(all_pages, embeddings) # setup svm retriever
-    knowledgebase = svm_retriever.get_relevant_documents(user_question) # find relevant pages
-    print("There are ", len(knowledgebase), "relevant pages found")
-
 
 # function to store the conversation history
 def update_conversation_history(user_question, chatbot_response):
@@ -86,11 +41,50 @@ def update_conversation_history(user_question, chatbot_response):
     conversation_history += "Makkie responded: " + chatbot_response + "\n\n"
     return conversation_history
 
-# # Load database
-# loader = PyPDFLoader("./New_Employee_Orientation_Document.pdf")
-# # split into pages
-# all_pages = loader.load_and_split()
-# # store as vector database
-# vectorstore = Chroma.from_documents(documents=all_pages, embedding=embeddings)
-# print("There are", len(all_pages), "pages sucessfully stored in vector database")
+
+def load_database():
+    # # Load database
+    loader = PyPDFLoader("./Dataset4.pdf")
+    # # split into pages
+    all_pages = loader.load_and_split()
+    # # store as vector database
+    # vectorstore = Chroma.from_documents(documents=all_pages, embedding=embeddings)
+    print("There are", len(all_pages), "pages sucessfully stored in vector database")
+    return all_pages
+
+# def find_pages(all_pages, user_question):
+#     # find relevant pages
+#     svm_retriever = SVMRetriever.from_documents(all_pages ,embeddings) # setup svm retriever
+#     knowledgebase = svm_retriever.get_relevant_documents(user_question) # find relevant pages
+#     print("There are ", len(knowledgebase), "relevant pages found")
+#     return knowledgebase
+
+def generate_prompt(prompt):
+    rag_prompt_custom = PromptTemplate.from_template(prompt)
+    return rag_prompt_custom
+
+def model_call(prompt, all_pages, model):
+    model(
+        [
+            HumanMessage(
+                content=prompt(user_question, all_pages, conversation_history)
+            )
+        ]
+    )
+    response = model(
+    [
+        HumanMessage(
+            content="Translate this sentence from English to French. I love programming."
+        )
+    ]
+    ) 
+
+def generate_prompt_and_call(rag_prompt_custom, all_pages, model):
+    rag_chain = (
+        {"knowledgebase": all_pages, "user_question": RunnablePassthrough(), "conversation_history": RunnablePassthrough()} 
+        | rag_prompt_custom 
+        | model
+    )
+    return rag_chain   
+
 
